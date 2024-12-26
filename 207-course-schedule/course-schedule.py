3278028cf0cc10616ed6428b9c843a2b33ac02c7
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-26 03:58:09 -0500

    Time: 4 ms (69.97%) | Memory: 19.7 MB (5.31%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,55 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # if not prerequisites:
+            # return True
+
+        # indeg = [0]*numCourses
+        # adj = {i:[] for i in range(numCourses)}
+        # for course, prereq in prerequisites:
+        #     adj[prereq].append(course)
+        #     indeg[course] += 1
+
+        # q = deque()
+        # done = 0
+        # for i in range(numCourses):
+        #     if indeg[i]==0:
+        #         q.append(i)
+        # while q:
+        #     n= len(q)
+        #     for _ in range(n):
+        #         curr = q.popleft()
+        #         done += 1
+        #         for nei in adj[curr]:
+        #             indeg[nei] -= 1
+        #             if indeg[nei]==0:
+        #                 q.append(nei)
+
+        # return done == numCourses
+
+        if not prerequisites:
+            return True
+
+        adj = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            adj[prereq].append(course)
+
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return False
+            if not adj[node]:
+                return True
+            
+            visited.add(node)
+            for nei in adj[node]:
+                if not dfs(nei):
+                    return False
+
+            visited.remove(node)
+            adj[node] = []
+            return True
+            
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
