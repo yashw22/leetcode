6a666b1eafaacd917e50fe40ed74b0213c1e65c1
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-26 05:56:17 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (7.56%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        even = [0]*n
+
+        def bfs(node):
+            if even[node]:
+                return True
+
+            q = deque([node])
+            even[node] = 1
+
+            while q:
+                curr = q.popleft()
+                for nei in graph[curr]:
+                    if even[curr]==even[nei]:
+                        return False
+                    elif not even[nei]:
+                        q.append(nei)
+                        even[nei] = -1 * even[curr]
+            return True
+
+
+        for i in range(n):
+            if not bfs(i):
+                return False
+        return True
