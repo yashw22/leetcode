4ad2797150264b8ba97a0e17e8572b80f5c53354
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-26 06:04:38 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (7.56%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,26 +1,48 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        # -------bfs--------
+        # n = len(graph)
+        # even = [0]*n
+
+        # def bfs(node):
+        #     if even[node]:
+        #         return True
+
+        #     q = deque([node])
+        #     even[node] = 1
+
+        #     while q:
+        #         curr = q.popleft()
+        #         for nei in graph[curr]:
+        #             if even[curr]==even[nei]:
+        #                 return False
+        #             elif not even[nei]:
+        #                 q.append(nei)
+        #                 even[nei] = -1 * even[curr]
+        #     return True
+
+        # for i in range(n):
+        #     if not bfs(i):
+        #         return False
+        # return True
+
+        # --------dfs---------
         n = len(graph)
         even = [0]*n
 
-        def bfs(node):
+        def dfs(node, isEven):
             if even[node]:
                 return True
+            
+            even[node] = 1 if isEven else -1
 
-            q = deque([node])
-            even[node] = 1
-
-            while q:
-                curr = q.popleft()
-                for nei in graph[curr]:
-                    if even[curr]==even[nei]:
-                        return False
-                    elif not even[nei]:
-                        q.append(nei)
-                        even[nei] = -1 * even[curr]
+            for nei in graph[node]:
+                if even[nei]==even[node] or not dfs(nei, not isEven):
+                    return False
             return True
-
+            
+        
         for i in range(n):
-            if not bfs(i):
+            if not dfs(i, True):
                 return False
         return True
