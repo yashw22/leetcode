43aaadb87afa2ccc242bd62d08c041ba1aab489c
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-26 00:10:44 -0500

    Time: 190 ms (32.51%) | Memory: 38.4 MB (28.56%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,14 +1,38 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        cache = {}
+        # cache = {}
 
-        def dfs(i, tot):
-            if i==len(nums):
-                return 1 if tot==target else 0
-            if (i, tot) in cache:
-                return cache[(i,tot)]
+        # def dfs(i, tot):
+        #     if i==len(nums):
+        #         return 1 if tot==target else 0
+        #     if (i, tot) in cache:
+        #         return cache[(i,tot)]
             
-            cache[(i,tot)] = dfs(i+1, tot+nums[i]) + dfs(i+1, tot-nums[i])
-            return cache[(i,tot)]
+        #     cache[(i,tot)] = dfs(i+1, tot+nums[i]) + dfs(i+1, tot-nums[i])
+        #     return cache[(i,tot)]
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        n = len(nums)
+        mem = {}
+        
+        def dfs(i, curr):
+            if i==n:
+                return 1 if curr==target else 0
+
+            if (i,curr) in mem:
+                return mem[(i,curr)]
+
+            numWays = dfs(i+1, curr+nums[i])
+            numWays += dfs(i+1, curr-nums[i])
+            mem[(i,curr)] = numWays
+
+            return numWays
+
+        return dfs(0,0)
+
+                
+
+        
+
+
