55330403fdcda3658347b65028c7dba90922e401
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-27 00:14:23 -0500

    Time: 3 ms (6.74%) | Memory: 17.9 MB (5.95%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        idx = -1
+        for i in range(n-1, 0, -1):
+            if nums[i] > nums[i-1]:
+                idx = i-1
+                break
+
+        if idx==-1:
+            for i in range(floor(n/2)):
+                nums[i], nums[n-i-1] = nums[n-i-1], nums[i]
+            return
+
+        print(idx)
+
+        low = idx+1
+        for i in range(idx+1, n):
+            if nums[idx]<nums[i]<nums[low]:
+                low = i
+
+        nums[idx], nums[low] = nums[low], nums[idx]
+        nums[idx+1:] = sorted(nums[idx+1:])
