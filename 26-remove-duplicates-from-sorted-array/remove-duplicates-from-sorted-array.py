95235725f119932848382bbde021100b60ca358e
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-31 19:06:34 -0500

    Time: 4 ms (29.28%) | Memory: 18.8 MB (18.06%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,13 +4,10 @@ class Solution:
         i, j = 1, 1
 
         while j<n:
-            if nums[j]==nums[j-1]:
-                j += 1
-                continue
-            else:
+            if nums[j]!=nums[j-1]:
                 nums[i] = nums[j]
                 i += 1
-                j +=1
+            j += 1
 
         return i
         
