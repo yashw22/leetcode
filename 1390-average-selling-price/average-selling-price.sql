e8c1916f706cff4e52713ae558383510d866bfc7
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-01-17 11:51:14 -0500

    Time: 2214 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -8,27 +8,28 @@
 -- on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date
 -- group by p.product_id; 
 
-with up as(
-    select product_id, round(sum(cost)/sum(units),2) as average_price
-    from (
-        select u.product_id, u.units, u.units*p.price as cost
-        from UnitsSold u
-        left join Prices p
-        on u.product_id=p.product_id
-        where u.purchase_date between p.start_date and p.end_date
-    ) as pp
-    group by product_id
-),
-cp as (
-    select distinct product_id
-    from Prices
-)
-select cp.product_id, coalesce(up.average_price,0) as average_price
-from cp
-left join up
-on cp.product_id=up.product_id
--- select p.product_id
--- from Prices p
+-- with up as(
+--     select product_id, round(sum(cost)/sum(units),2) as average_price
+--     from (
+--         select u.product_id, u.units, u.units*p.price as cost
+--         from UnitsSold u
+--         left join Prices p
+--         on u.product_id=p.product_id
+--         where u.purchase_date between p.start_date and p.end_date
+--     ) as pp
+--     group by product_id
+-- ),
+-- cp as (
+--     select distinct product_id
+--     from Prices
+-- )
+-- select cp.product_id, coalesce(up.average_price,0) as average_price
+-- from cp
 -- left join up
--- on p.product_id=up.product_id
--- group by p.product_id;
+-- on cp.product_id=up.product_id;
+
+select p.product_id, round( coalesce(sum(p.price*u.units)/sum(u.units),0), 2) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date
+group by product_id
