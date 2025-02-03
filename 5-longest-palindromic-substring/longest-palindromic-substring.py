4bd3cc398542eb6c2bc073bba74e3b4d9f60ae11
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-02-02 22:54:37 -0500

    Time: 207 ms (94.26%) | Memory: 17.9 MB (54.02%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,68 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # n = len(s)
+        # if n==1:
+        #     return s
+
+        # mx = 1
+        # mi,mj = 0,0
+
+        # def isPalin(x,y):
+        #     while x<y:
+        #         if s[x]!=s[y]:
+        #             return False
+        #         x+=1
+        #         y-=1
+        #     return True
+
+        # for i in range(0, n-1):
+        #     for j in range(i,n):
+        #         if isPalin(i,j) and (j-i+1)>mx:
+        #             mx = j-i+1
+        #             mi,mj = i,j
+
+        # return s[mi:mj+1]
+
+        # res = ""
+        # resLen = 0
+        # for i in range(len(s)):
+        #     l, r = i,i
+        #     while l>=0 and r<len(s) and s[l]==s[r]:
+        #         if (r-l+1) > resLen:
+        #             res = s[l:r+1]
+        #             resLen = r-l+1
+        #         l -=1
+        #         r +=1
+
+        #     l, r = i,i+1
+        #     while l>=0 and r<len(s) and s[l]==s[r]:
+        #         if (r-l+1) > resLen:
+        #             res = s[l:r+1]
+        #             resLen = r-l+1
+        #         l -=1
+        #         r +=1
+
+        # return res
+
+        def getPalin(l, r, s, n):
+            while l>=0 and r<n and s[l]==s[r]:
+                l -= 1
+                r += 1
+            return r-l+1-2, s[l+1: r]
+
+
+        res = ""
+        reslen = 0
+        n = len(s)
+        for i in range(n):
+            currlen, currPalin = getPalin(i, i, s, n)
+            if currlen>reslen:
+                reslen = currlen
+                res = currPalin
+            
+            currlen, currPalin = getPalin(i, i+1, s, n)
+            if currlen>reslen:
+                reslen = currlen
+                res = currPalin
+            
+        return res
