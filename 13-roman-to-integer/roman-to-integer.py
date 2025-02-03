1719409c9ab0e80b79c898aa54774f54e3548a63
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-02-03 00:40:27 -0500

    Time: 10 ms (18.59%) | Memory: 17.8 MB (53.00%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -31,12 +31,13 @@ class Solution:
         # return res
 
         romanMap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        subtractMap = {'I': ['V', 'X'], 'X': ['L','C'], 'C': ['D', 'M']}
+        # subtractMap = {'I': ['V', 'X'], 'X': ['L','C'], 'C': ['D', 'M']}
 
         res = 0
         n = len(s)
         for i in range(n):
-            if i!=n-1 and s[i] in subtractMap and s[i+1] in subtractMap[s[i]]:
+            # if i!=n-1 and s[i] in subtractMap and s[i+1] in subtractMap[s[i]]:
+            if i!=n-1 and romanMap[s[i+1]]>romanMap[s[i]]:
                 res -= romanMap[s[i]]
             else:
                 res += romanMap[s[i]]
