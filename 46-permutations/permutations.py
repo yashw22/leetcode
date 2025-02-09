a30e5b25eaa7e00f2d438779db57e180daf8f4bc
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-02-09 18:02:00 -0500

    Time: 3 ms (33.79%) | Memory: 18.1 MB (29.58%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,44 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # res = []
+        # n = len(nums)
+        # # picked = int(0)
+        # curr = []
+
+        # def dfs(picked):
+        #     if len(curr) == n:
+        #         res.append(curr.copy())
+        #         return
+
+        #     for i in range(n):
+        #         if not (picked & (1<<i)):
+        #             curr.append(nums[i])
+        #             # picked |= 1<<i
+        #             dfs(picked | (1<<i))
+        #             curr.pop()
+        #             # picked &= ~(1<<i)
+                
+        # dfs(0)
+        # return res
+
+
+        res = []
+        curr = []
+        visit = set()
+
+        def rec():
+            if len(curr) == len(nums):
+                res.append(curr[:])
+                return
+            
+            for i in range(len(nums)):
+                if i not in visit:
+                    curr.append(nums[i])
+                    visit.add(i)
+                    rec()
+                    visit.remove(i)
+                    curr.pop()
+            
+
+        rec()
+        return res
