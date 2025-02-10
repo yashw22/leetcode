e9731acf7b60d47f312addbbf8c765401fd0c952
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-02-09 22:59:02 -0500

    Time: 7 ms (49.77%) | Memory: 18.9 MB (89.64%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -26,30 +26,64 @@ class Solution:
 
         # return done == numCourses
 
+        # if not prerequisites:
+        #     return True
+
+        # adj = {i: [] for i in range(numCourses)}
+        # for course, prereq in prerequisites:
+        #     adj[prereq].append(course)
+
+        # visited = set()
+        # def dfs(node):
+        #     if node in visited:
+        #         return False
+        #     if not adj[node]:
+        #         return True
+            
+        #     visited.add(node)
+        #     for nei in adj[node]:
+        #         if not dfs(nei):
+        #             return False
+
+        #     visited.remove(node)
+        #     adj[node] = []
+        #     return True
+            
+        # for i in range(numCourses):
+        #     if not dfs(i):
+        #         return False
+        # return True
+
         if not prerequisites:
             return True
+        
+        adj = {i:[] for i in range(numCourses)}
+        indeg = [0]*numCourses
+        for u, v in prerequisites:
+            adj[v].append(u)
+            indeg[u] += 1
 
-        adj = {i: [] for i in range(numCourses)}
-        for course, prereq in prerequisites:
-            adj[prereq].append(course)
-
-        visited = set()
-        def dfs(node):
-            if node in visited:
-                return False
-            if not adj[node]:
-                return True
-            
-            visited.add(node)
-            for nei in adj[node]:
-                if not dfs(nei):
-                    return False
-
-            visited.remove(node)
-            adj[node] = []
-            return True
-            
+        q = deque()
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+            if indeg[i]==0:
+                    q.append(i)
+
+        done = 0
+        while q:
+            n = len(q)
+            for _ in range(n):
+                curr = q.popleft()
+                done += 1
+                for nei in adj[curr]:
+                    indeg[nei] -= 1
+                    if indeg[nei]==0:
+                        q.append(nei)
+        
+        return done==numCourses
+                
+
+
+
+            
+            
+
