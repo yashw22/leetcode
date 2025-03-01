ba3657cb9eaed0a4d70e89f09d8cfffb996254f8
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2025-03-01 01:20:34 -0500

    Time: 55 ms (87.01%) | Memory: 20.8 MB (48.61%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,69 +1,67 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # adj = defaultdict(set)
+        # if endWord not in wordList:
+        #     return 0
 
-        # def isNei(w1,w2):
-        #     l = len(w1)
-        #     ct = 0
-        #     for i in range(l):
-        #         if w1[i]==w2[i]:
-        #             ct+=1
-        #     return True if ct+1==l else False
+        # wlen = len(beginWord)
+        # nei = defaultdict(list)
 
-        # for word1 in wordList + [beginWord]:
-        #     for word2 in wordList+ [beginWord]:
-        #         if word1!=word2 and isNei(word1, word2):
-        #             adj[word1].add(word2)
-        #             adj[word2].add(word1)
+        # wordList.append(beginWord)
+        # for word in wordList:
+        #     for j in range(wlen):
+        #         pattern = word[:j] + "*" + word[j+1:]
+        #         nei[pattern].append(word)
 
-        # visited = set()
-        # q = deque([endWord])
-
+        # visit = set([beginWord])
+        # q = deque([beginWord])
         # res = 1
-        # while(q):
-        #     n = len(q)
-        #     res +=1
-        #     for _ in range(n):
-        #         node = q.popleft()
-        #         visited.add(node)
-        #         for nei in adj[node]:
-        #             if nei==beginWord:
-        #                 return res
-        #             if nei not in visited:
-        #                 q.append(nei)
 
+        # while q:
+        #     n = len(q)
+        #     for _ in range(n):
+        #         word = q.popleft()
+        #         if word==endWord:
+        #             return res
+        #         for j in range(wlen):
+        #             pattern = word[:j] + "*" + word[j+1:]
+        #             for neiWord in nei[pattern]:
+        #                 if neiWord not in visit:
+        #                     q.append(neiWord)
+        #                     visit.add(neiWord)
+
+                
+                
+        #     res +=1
         # return 0
 
         if endWord not in wordList:
             return 0
 
-        wlen = len(beginWord)
+        wl = len(beginWord)
         nei = defaultdict(list)
 
         wordList.append(beginWord)
-        for word in wordList:
-            for j in range(wlen):
-                pattern = word[:j] + "*" + word[j+1:]
-                nei[pattern].append(word)
-
+        for wrd in wordList:
+            for i in range(wl):
+                pattern = wrd[:i]+"*"+wrd[i+1:]
+                nei[pattern].append(wrd)
+        
         visit = set([beginWord])
         q = deque([beginWord])
         res = 1
 
         while q:
-            n = len(q)
-            for _ in range(n):
-                word = q.popleft()
-                if word==endWord:
+            ql = len(q)
+            for _ in range(ql):
+                wrd = q.popleft()
+                if wrd==endWord:
                     return res
-                for j in range(wlen):
-                    pattern = word[:j] + "*" + word[j+1:]
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visit:
-                            q.append(neiWord)
-                            visit.add(neiWord)
+                for i in range(wl):
+                    pattern = wrd[:i]+'*'+wrd[i+1:]
+                    for neiWrd in nei[pattern]:
+                        if neiWrd not in visit:
+                            q.append(neiWrd)
+                            visit.add(neiWrd)
+            res+=1
 
-                
-                
-            res +=1
         return 0
